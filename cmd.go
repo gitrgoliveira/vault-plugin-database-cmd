69f909c252191ddb2227c29eb61dc7357966de76
Ricardo Oliveira <ricardo@hashicorp.com>
2025-01-14 16:19:56 +0000

    feat: enhance user management commands with logging and script execution

--- cmd.go
+++ cmd.go
@@ -2,58 +2,211 @@ package cmd
 
 import (
 	"context"
+	"crypto/tls"
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
 
+	hclog "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-secure-stdlib/strutil"
 	dbplugin "github.com/hashicorp/vault/sdk/database/dbplugin/v5"
+	"github.com/hashicorp/vault/sdk/database/helper/credsutil"
+	identity "github.com/hashicorp/vault/sdk/helper/pluginidentityutil"
+	"github.com/mitchellh/mapstructure"
+)
+
+const (
+	defaultTimeout = 20000 * time.Millisecond
+	maxKeyLength   = 64
 )
 
 var _ dbplugin.Database = (*cmd)(nil)
 
 type cmd struct {
-	// sync.RWMutex
+	Logger hclog.Logger
 
-	// usernameProducer template.StringTemplate
-	// credsutil.CredentialsProducer
+	Config map[string]string
+	credsutil.CredentialsProducer
+
+	RawConfig map[string]interface{}
+
+	RootTLSConfig *tls.Config
+
+	RootUsername    string `json:"username"`
+	RootPassword    string `json:"password"`
+	RootCertificate string `json:"certificate"`
+
+	identity.PluginIdentityTokenParams
 }
 
 func New() (interface{}, error) {
 	db := new()
 
 	// This middleware isn't strictly required, but highly recommended to prevent accidentally exposing
-	// values such as passwords in error messages. An example of this is included below
+	// values such as passwords in error messages.
 	dbType := dbplugin.NewDatabaseErrorSanitizerMiddleware(db, db.secretValues)
 	return dbType, nil
 }
 
 func new() *cmd {
-	// ...
-	db := &cmd{
-		// ...
-	}
+	db := &cmd{}
+	db.Logger = hclog.New(&hclog.LoggerOptions{})
 	return db
 }
 
 func (db *cmd) secretValues() map[string]string {
 	return map[string]string{
-		"db.password": "[password]",
+		"password": "[password]",
 	}
 }
 
 func (db *cmd) Initialize(ctx context.Context, req dbplugin.InitializeRequest) (dbplugin.InitializeResponse, error) {
+	db.Logger = hclog.New(&hclog.LoggerOptions{})
+
+	db.Logger.Info("Initialize", "config", req.Config)
+
+	db.RawConfig = req.Config
+	decoderConfig := &mapstructure.DecoderConfig{
+		Result:           db,
+		WeaklyTypedInput: true,
+		TagName:          "json",
+	}
+
+	decoder, err := mapstructure.NewDecoder(decoderConfig)
+	if err != nil {
+		return dbplugin.InitializeResponse{}, err
+	}
+
+	err = decoder.Decode(req.Config)
+	if err != nil {
+		return dbplugin.InitializeResponse{}, err
+	}
+
+	// username password
+	RootUsername, err := strutil.GetString(req.Config, "root_username")
+	if err != nil {
+		return dbplugin.InitializeResponse{}, fmt.Errorf("failed to retrieve root_username: %w", err)
+	}
+	db.RootUsername = RootUsername
+	RootPassword, err := strutil.GetString(req.Config, "root_password")
+	if err != nil {
+		return dbplugin.InitializeResponse{}, fmt.Errorf("failed to retrieve root_password: %w", err)
+	}
+	db.RootPassword = RootPassword
+	// and TLS config/certs
+
 	resp := dbplugin.InitializeResponse{
 		Config: req.Config,
 	}
+
+	resp.SetSupportedCredentialTypes([]dbplugin.CredentialType{
+		dbplugin.CredentialTypePassword,
+	})
+
 	return resp, nil
 }
 
 func (db *cmd) NewUser(ctx context.Context, req dbplugin.NewUserRequest) (dbplugin.NewUserResponse, error) {
-	return dbplugin.NewUserResponse{}, nil
+	db.Logger.Info("NewUser", "statements", req.Statements.Commands)
+
+	// These statements are not used. They are only for the purpose of this example.
+	db.Logger.Info("NewUser", "rollback_statements", req.RollbackStatements.Commands)
+
+	if req.CredentialType == dbplugin.CredentialTypePassword {
+		db.Logger.Info("NewUser", "curr_password", req.Password)
+		db.Logger.Info("NewUser", "username", req)
+
+		username, err := credsutil.GenerateUsername(
+			credsutil.DisplayName(req.UsernameConfig.DisplayName, maxKeyLength),
+			credsutil.RoleName(req.UsernameConfig.RoleName, maxKeyLength))
+
+		if err != nil {
+			return dbplugin.NewUserResponse{}, fmt.Errorf("failed to generate username: %w", err)
+		}
+
+		db.Logger.Info("NewUser", "future_username", username)
+
+		// Assemble rollback statements into a single script
+		script := strings.Join(req.Statements.Commands, "\n")
+		params := map[string]string{
+			"name":       username,
+			"username":   username,
+			"password":   req.Password,
+			"expiration": req.Expiration.String(),
+		}
+
+		renderedScript := replaceVars(params, script)
+
+		// Execute the script
+		cmd := exec.Command("/bin/bash", "-c", renderedScript)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			return dbplugin.NewUserResponse{}, fmt.Errorf("failed to execute rollback script: %s, error: %w", script, err)
+		}
+		db.Logger.Info("Executed creation script", "script", script, "output", string(output))
+
+		return dbplugin.NewUserResponse{
+			Username: username,
+		}, nil
+
+	}
+
+	return dbplugin.NewUserResponse{}, fmt.Errorf("only 'password' credential type is supported. Got %s ", req.CredentialType)
 }
 
 func (db *cmd) UpdateUser(ctx context.Context, req dbplugin.UpdateUserRequest) (dbplugin.UpdateUserResponse, error) {
-	return dbplugin.UpdateUserResponse{}, nil
+	if req.CredentialType == dbplugin.CredentialTypePassword {
+		db.Logger.Info("UpdateUser", "username", req.Username)
+		db.Logger.Info("UpdateUser", "password_statements", req.Password.Statements.Commands)
+
+		// These statements are not used. They are only for the purpose of this example.
+		db.Logger.Info("UpdateUser", "expiration_statements", req.Expiration.Statements.Commands)
+
+		// Assemble password change statements into a single script
+		script := strings.Join(req.Password.Statements.Commands, "\n")
+		params := map[string]string{
+			"name":       req.Username,
+			"username":   req.Username,
+			"password":   req.Password.NewPassword,
+			"expiration": req.Expiration.NewExpiration.String(),
+		}
+		renderedScript := replaceVars(params, script)
+
+		// Execute the script
+		cmd := exec.Command("/bin/bash", "-c", renderedScript)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			return dbplugin.UpdateUserResponse{}, fmt.Errorf("failed to execute password change script: %s, error: %w", script, err)
+		}
+		db.Logger.Info("Executed password change script", "script", script, "output", string(output))
+
+		return dbplugin.UpdateUserResponse{}, nil
+	}
+
+	return dbplugin.UpdateUserResponse{}, fmt.Errorf("only 'password' credential type is supported. Got %s ", req.CredentialType)
 }
 
 func (db *cmd) DeleteUser(ctx context.Context, req dbplugin.DeleteUserRequest) (dbplugin.DeleteUserResponse, error) {
+	db.Logger.Info("DeleteUser", "username", req.Username)
+	db.Logger.Info("DeleteUser", "statements_commands", req.Statements.Commands)
+
+	// Assemble delete statements into a single script
+	script := strings.Join(req.Statements.Commands, "\n")
+	params := map[string]string{
+		"name":     req.Username,
+		"username": req.Username,
+	}
+	renderedScript := replaceVars(params, script)
+
+	// Execute the script
+	cmd := exec.Command("/bin/bash", "-c", renderedScript)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return dbplugin.DeleteUserResponse{}, fmt.Errorf("failed to execute delete script: %s, error: %w", script, err)
+	}
+	db.Logger.Info("Executed delete script", "script", script, "output", string(output))
+
 	return dbplugin.DeleteUserResponse{}, nil
 }
 
@@ -62,5 +215,17 @@ func (db *cmd) Type() (string, error) {
 }
 
 func (db *cmd) Close() error {
+	db.Logger.Info("Close", "closing", "cmd")
 	return nil
+}
+
+func replaceVars(m map[string]string, tpl string) string {
+	if m == nil || len(m) <= 0 {
+		return tpl
+	}
+
+	for k, v := range m {
+		tpl = strings.ReplaceAll(tpl, fmt.Sprintf("{{%s}}", k), v)
+	}
+	return tpl
 }
